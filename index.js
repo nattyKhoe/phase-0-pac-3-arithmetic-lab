9871d7efbe9424ba587bf99a6cc39c945b4f3459
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-21 14:33:41 +1000

    Done without problems.

---
+++ index.js
@@ -0,0 +1,24 @@
+function add (number1,number2){
+    return number1+number2;
+}
+function subtract (number1,number2){
+    return number1-number2;
+}
+function multiply(number1,number2){
+    return number1*number2;
+}
+function divide(number1,number2){
+    return number1/number2;
+}
+function increment(n){
+    return ++n;
+}
+function decrement (n){
+    return --n;
+}
+function makeInt (string) {
+    return parseInt(string,10);
+}
+function preserveDecimal(string) {
+    return parseFloat(string);
+}
